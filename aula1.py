3136ca383fcd1fc424cb1ce0466d71b08db2ad0d
paulo.souza <paulo_nunis2@hotmail.com>
2022-05-20 13:26:46 -0300

    Add aula do curso pyhton

---
+++ aula1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# importar a base de dados
+tabela_vendas = pd.read_excel('vendas.xlsx')
+
+print(tabela_vendas)
+
+# visualizar a base de dados
+
+# faturamento por loja
+
+# qtd de produto por loja
+
+# ticket medio por produto  em cada loja
+
+# enviar um email com o relatório
+
+
+
+
